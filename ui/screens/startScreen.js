f9f0b167101188ea76c8a77de68b00a034958c87
k0dekk <sashaoksiuk@gmail.com>
2026-04-17 13:59:30 +0300

    translate the startScreen and check the correctness of the code execution

--- ui/screens/startScreen.js
+++ ui/screens/startScreen.js
@@ -1,11 +1,12 @@
 import { render, on, fadeIn } from "../render.js";
+import { t } from "../../utils/i18n.js";
 
 export function showStartScreen(onContinue) {
   render(`
     <div>
       <h1>ALIAS</h1>
       <hr>
-      <button id="startBtn">Почати гру</button>
+      <button id="startBtn">${t("startScreen.startButton")}</button>
     </div>
   `);
 
