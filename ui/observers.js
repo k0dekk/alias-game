9275eb8d163e274f81def34b12476ea3fce5fb47
k0dekk <sashaoksiuk@gmail.com>
2026-05-19 17:32:31 +0300

    implement generic EventEmitter for cross-component communication

---
+++ ui/observers.js
@@ -0,0 +1,24 @@
+class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  subscribe(eventName, callback) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+    this.events[eventName].push(callback);
+
+    return () => {
+      this.events[eventName] = this.events[eventName].filter(cb => cb !== callback);
+    };
+  }
+
+  emit(eventName, data) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach(callback => callback(data));
+    }
+  }
+}
+
+export const uiEventBus = new EventEmitter();
