a66323ad0577af37d7ef8f69e3e4d04dd4def1dd
k0dekk <sashaoksiuk@gmail.com>
2026-05-19 16:53:05 +0300

    implement teamValidator base logic

--- utils/lab5/teamValidator.js
+++ utils/lab5/teamValidator.js
@@ -1,2 +1,36 @@
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "./firebase.js";
+
+export async function checkTeamNameValid(teamName, signal = null) {
+  // 1. перевірка на випадок, якщо AbortController спрацював на самому старті
+  if (signal?.aborted) {
+    throw new DOMException(`Валідацію скасовано`, 'AbortError');
+  }
+
+  // 2. шукаємо в колекції "banned_teams" документ, де поле "name" дорівнює введеній назві
+  const q = query(
+    collection(db, "banned_teams")
+  );
+
+  try {
+    // 3. робимо запит до серверів Firebase
+    const querySnapshot = await getDocs(q);
+
+    // 4. перевіряємо, чи юзер не скасував запит
+    if (signal?.aborted) {
+      throw new DOMException(`Валідацію скасовано`, 'AbortError');
+    }
+
+    // 5. Якщо querySnapshot не порожній, значить таке слово є в чорному списку
+    const isBanned = !querySnapshot.empty;
+    
+    console.log(`[Firestore] Перевірка '${teamName}' | Заборонено: ${isBanned}`);
+    return isBanned;
+
+  } catch (error) {
+    if (error.name === 'AbortError') throw error; 
+    
+    console.error(error);
+    return false; // У разі помилки мережі дозволяємо грати, щоб не ламати гру
+  }
+}
